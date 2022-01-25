0974893710dc0f2b1964245150ea21945347fa90
hudaokeji <github@hudaokeji.com>
2022-01-25 22:10:51 +0900

    init commit

---
+++ dist/skylark-prism/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-prism - A version of prism.js that ported to running on skylarkjs
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-prism/
+ * @license MIT
+ */
+define(["./prism"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./prism"
+],function(prism){
+	return prism;
+});
